43748fca89cb50f7aadc64ad05aae8490ab5d944
Dmitry Shcherbina <shcherbinadmitry@gmail.com>
2022-02-26 15:22:51 +0300

    tinkering with axios url params #2

--- src/loadFeed.js
+++ src/loadFeed.js
@@ -5,6 +5,9 @@ export default (feed) => {
   const path = new URL('https://hexlet-allorigins.herokuapp.com/get?disableCache=true');
   path.searchParams.set('disableCache', true);
   path.searchParams.set('url', feed.url);
+  console.log('Logging path obj');
+  console.log(path);
+  console.log(path.url);
 
   return axios.get(path.href).then((responce) => parse(responce.data.contents, feed));
 };
