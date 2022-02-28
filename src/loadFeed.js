004e3206e8714ee747263e9f37f5a09a413c4e46
Dmitry Shcherbina <shcherbinadmitry@gmail.com>
2022-02-28 17:39:13 +0300

    tinkering with url params #18

--- src/loadFeed.js
+++ src/loadFeed.js
@@ -3,17 +3,17 @@ import parse from './parser.js';
 
 const proxify = (link) => {
   const url = new URL('https://hexlet-allorigins.herokuapp.com/get');
-  url.searchParams.set('url', link);
-  url.searchParams.set('disableCache', 'true');
+  // url.searchParams.set('url', link);
+  // url.searchParams.set('disableCache', 'true');
 
-  return url;
+  return `${url.href}?disableCache=true&url=${link}`;
 };
 
 export default (url) => {
   const path = proxify(url);
   console.log('Logging path obj');
   console.log(path);
-  console.log(path.searchParams.get('url'));
+  // console.log(path.searchParams.get('url'));
 
   return axios.get(path).then((responce) => parse(responce.data.contents));
 };
